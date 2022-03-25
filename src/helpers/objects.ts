4392a147ed20cbe78439a40fd0a1f0c9f4709abd
Stevvvvvi <lly363893886@gmail.com>
2022-03-25 12:24:57 +1100

    add mist weather

--- src/helpers/objects.ts
+++ src/helpers/objects.ts
@@ -3,6 +3,6 @@ export const rainFontMapping=[
 	{main: "Haze", weatherFont: "wi-day-haze", background: "linear-gradient(180deg, #bfbfbf 0%, #ffffff 100%)"},
 	{main: "Clear", weatherFont: "wi-day-sunny", background: "linear-gradient(180deg, #87CEEB 0%, #ffffff 100%)"},
 	{main: "Clouds", weatherFont: "wi-day-cloudy-high", background: "linear-gradient(180deg, #8f8f8f 0%, #a6a6a6 100%)"},
-	{main: "Snow", weatherFont: "wi-day-snow", background: "linear-gradient(180deg, #e0e0e0 0%, #ffffff 100%)"}
-    
+	{main: "Snow", weatherFont: "wi-day-snow", background: "linear-gradient(180deg, #e0e0e0 0%, #ffffff 100%)"},
+	{main: "Mist", weatherFont: "wi-day-fog", background: "linear-gradient(180deg, #ffffff 0%, #949494 100%)"}
 ];
